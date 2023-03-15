d05ab8d0f76313088310bac32c0672e093243347
Kageetai <ma@kageetai.net>
2023-03-15 12:55:02 +0100

    github-mark-viewed.js: add more meta data

--- github-mark-viewed.js
+++ github-mark-viewed.js
@@ -1,14 +1,18 @@
 // ==UserScript==
-// @name        GitHub mark as viewed
-// @namespace   http://tampermonkey.net/
-// @version     0.1
-// @description marks GitHub PR files as "viewed" when clicking Alt+V
-// @author      Kageetai
-// @match       https://github.com/*/pull/*/files*
-// @icon        https://www.google.com/s2/favicons?sz=64&domain=github.com
-// @grant       none
-// @licence     MIT
-// @copyright   2023, Kageetai (https://openuserjs.org/users/Kageetai)
+// @name            GitHub mark as viewed
+// @namespace       http://tampermonkey.net/
+// @version         1.0.0
+// @description     marks GitHub PR files as "viewed" when clicking Alt+V
+// @author          Kageetai
+// @homepageUrl     https://github.com/Kageetai/TamperMonkey
+// @contributionUrl https://github.com/Kageetai/TamperMonkey
+// @supportUrl      https://github.com/Kageetai/TamperMonkey/issues
+// @updateURL       https://openuserjs.org/meta/Kageetai/github-mark-viewed.meta.js
+// @match           https://github.com/*/pull/*/files*
+// @icon            https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant           none
+// @license         MIT
+// @copyright       2023, Kageetai (https://openuserjs.org/users/Kageetai)
 // ==/UserScript==
 
 (function () {
