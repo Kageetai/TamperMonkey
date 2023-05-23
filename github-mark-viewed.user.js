7ad4e2100ab9bbb45368c86c1fe2b470f190f97c
Kageetai <ma@kageetai.net>
2023-05-23 14:44:49 +0200

    github-mark-viewed.user.js: arrow functions

--- github-mark-viewed.user.js
+++ github-mark-viewed.user.js
@@ -15,21 +15,19 @@
 // @copyright       2023, Kageetai (https://openuserjs.org/users/Kageetai)
 // ==/UserScript==
 
-(function () {
+(() => {
     'use strict';
 
     // IntersectionObserver to detect when a button is visible and add class to it
-    const observer = new IntersectionObserver(function (entries) {
-        return entries.forEach(function (entry) {
-            const elem = entry.target;
+    const observer = new IntersectionObserver(entries => entries.forEach(entry => {
+        const elem = entry.target;
 
-            if (entry.isIntersecting && entry.intersectionRatio >= 1) {
-                elem.classList.add('isVisible');
-            } else {
-                elem.classList.remove('isVisible');
-            }
-        });
-    }, {threshold: 1});
+        if (entry.isIntersecting && entry.intersectionRatio >= 1) {
+            elem.classList.add('isVisible');
+        } else {
+            elem.classList.remove('isVisible');
+        }
+    }), {threshold: 1});
 
     const observeButtons = () => {
         // wait if GitHub is still loading more diffs
@@ -46,7 +44,7 @@
 
 
     // keyboard shortcut event listener
-    document.addEventListener('keyup', function (event) {
+    document.addEventListener('keyup', event => {
         if (event.altKey && event.code === 'KeyV') {
             const e = document.querySelector('input[value="viewed"]:not(:checked).isVisible:first-child');
 
